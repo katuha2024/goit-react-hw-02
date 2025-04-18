9c5d28ff98dcd503acfdc8b297209c7a573fcf7a
katuha2024 <ukrbrend2020@gmail.com>
2025-04-18 16:38:11 +0300

    add+

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import Description from '../components/description/Description';
+import css from './App.module.css';
 
 function App() {
-  const [feedback, setfeedback] = useState({
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0
   });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  return (
+    <div className={css.app}>
+      <Description />
+      <Options updateFeedback={updateFeedback} />
+    </div>
+  );
 }
 
-export default App
+export default App;
